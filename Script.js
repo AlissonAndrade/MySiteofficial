aaf6f1d665bb2490201821f6117666f4c3268565
AlissonAndrade <136262192+AlissonAndrade@users.noreply.github.com>
2023-07-26 22:55:53 -0300

    noar

---
+++ Script.js
@@ -0,0 +1,21 @@
+let i = document.getElementById('insta')
+let f = document.getElementById('face')
+let t = document.getElementById('twtt') 
+let g = document.getElementById('git')
+
+function Instagram () {
+    window.alert('Instagram...')
+}
+
+function Facebook () {
+    window.alert('Desculpe, não tenho Facebook!')
+}
+
+function Twitter () {
+    window.alert('Desculpe, não tenho Twitter!')
+}
+
+function Github () {
+    window.alert('https://github.com/AlissonAndrade')
+}
+
